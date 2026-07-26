4b756d3dc7488f4fed1e7dcd4741faa9b9506afa
Iskandar Isabekov <iskandarisabrkov@gmail.com>
2026-07-26 13:26:18 +0500

    feat: users model migrataion

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+// #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -29,4 +29,16 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    protected $fillable = [
+        'username',
+        'phone',
+        'email',
+        'password',
+        'telegram_id',
+        'image',
+        'cover_image',
+        'plan',
+        'bio',
+    ];
 }
